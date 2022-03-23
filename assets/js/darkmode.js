e37136271130025a3bb8139deddf9463bc29355e
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-03-23 09:45:32 -0400

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
 function setFlickerAnimation() {
   // get all elements that should be animated
   const animatedElements = Array.from(
-    document.querySelectorAll("h1.flicker")
+    document.querySelectorAll("h1")
   );
 
   if (!animatedElements.length) {
@@ -52,7 +52,7 @@ function setFlickerAnimation() {
       // make sure we don't wrap a space or punctuation char
       // or hit the same letter twice
       if (!excludedIndexes.includes(randIndex) && !excludedChars.includes(c)) {
-        chars[randIndex] = `<span class="h1.flicker">${c}</span>`;
+        chars[randIndex] = `<span class="h1">${c}</span>`;
         excludedIndexes.push(randIndex);
         i++;
       }
